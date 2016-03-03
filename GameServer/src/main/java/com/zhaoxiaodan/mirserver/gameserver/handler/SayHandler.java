026b8cba30686b33fe37a50f6540153f4362d74d
pangliang <418094911@qq.com>
2016-03-04 00:45:48 +0800

    把npc排一排看看外观

--- GameServer/src/main/java/com/zhaoxiaodan/mirserver/gameserver/handler/SayHandler.java
+++ GameServer/src/main/java/com/zhaoxiaodan/mirserver/gameserver/handler/SayHandler.java
@@ -6,25 +6,33 @@ import com.zhaoxiaodan.mirserver.network.packets.ClientPacket;
 import com.zhaoxiaodan.mirserver.network.packets.ServerPacket;
 import com.zhaoxiaodan.mirserver.utils.NumUtil;
 
-import java.util.Random;
-
 public class SayHandler extends PlayerHandler {
 
 
 	static int r = 0;
-	static short x = 289;
-	static short y = 618;
+	static short x = 280;
+	static short y = 610;
 
 	@Override
 	public void onPacket(ClientPacket packet, Player player) throws Exception {
 
-		x = (short) (x > 289+10 ? 289:x+1);
-		y = x == 289 ? (short) (y + 1) :y;
+		r=0;
+		x = (short) (player.currMapPoint.x+1);
+		y = player.currMapPoint.y;
+		for(int i=0 ;i<100;i++){
+//			x = (short) (280 + i % 10);
+//			y = (short) (610 + i/10);
 
-		short i = (short) (5 - new Random().nextInt(10));
-		int id = NumUtil.newAtomicId();
-		session.writeAndFlush(new ServerPacket.Turn(id, Direction.LEFT, x, y, 2,0,(byte)0,"HelloWord/255"));
-//		session.writeAndFlush(new ServerPacket.FeatureChanged(id, NumUtil.makeLong(0, r), (short) 0));
+			x++;
+
+			int id = NumUtil.newAtomicId();
+
+			int f =  NumUtil.makeLong(NumUtil.makeWord((byte)50, (byte)0), r++);
+
+			session.writeAndFlush(new ServerPacket.Turn(id, Direction.LEFT, x, y, f,0,(byte)3,r+"/255"));
+//			session.writeAndFlush(new ServerPacket.FeatureChanged(1000, f, (short) 0));
+		}
+
 
 	}
 }
