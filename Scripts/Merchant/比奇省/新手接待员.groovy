c1c629f0882af23152e6a8e6b342272eaf9b3ce8
pangliang <418094911@qq.com>
2016-03-07 15:54:48 +0800

    脚本函数名居然可以用中文...

--- Scripts/Merchant/比奇省/新手接待员.groovy
+++ Scripts/Merchant/比奇省/新手接待员.groovy
@@ -4,12 +4,12 @@ import com.zhaoxiaodan.mirserver.gameserver.engine.MapEngine
 
 public void main(Merchant merchant, Player player){
     String msg =  "欢迎来到 {哈哈呵呵/SCOLOR=253}\\ \\ \\" +
-            " <领取新手药包/@getNewPlayerItems>    <传送到新手练级地图/@moveToNewPlayerMap> \\ \\" +
+            " <领取新手药包/@getNewPlayerItems>    <传送到新手练级地图/@传送到新手练级地图> \\ \\" +
             " <离开/@exit>"
 
     merchant.sayTo(msg,player);
 }
 
-public void moveToNewPlayerMap(Merchant merchant, Player player){
+public void 传送到新手练级地图(Merchant merchant, Player player){
     MapEngine.enter(player,"G003");
 }
